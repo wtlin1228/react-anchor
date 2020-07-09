b6f07baf67348af9ac4333c202618dbeca501b95
wtlin1228 <wtlin1228@gmail.com>
2020-07-09 12:02:08 +0800

    Updates

---
+++ precache-manifest.81991845452792afaeec02024c1aa288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a596a30c280d57d5cabf9ca29b821ac1",
+    "url": "/react-anchor/index.html"
+  },
+  {
+    "revision": "7a6cc458dcfb45ac13e2",
+    "url": "/react-anchor/static/css/main.b795c629.chunk.css"
+  },
+  {
+    "revision": "9db06d74b41f7ec849bb",
+    "url": "/react-anchor/static/js/2.e9e4ba35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-anchor/static/js/2.e9e4ba35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6cc458dcfb45ac13e2",
+    "url": "/react-anchor/static/js/main.d37c804c.chunk.js"
+  },
+  {
+    "revision": "72b6bcd76f49bd4e52e7",
+    "url": "/react-anchor/static/js/runtime-main.eee2a724.js"
+  }
+]);
